d1de5703fa2bfe9875eb68e18db7c310dd4feeae
AustinConner <a.l.conner.2002@gmail.com>
2026-02-28 11:33:49 -0700

    frontend2

--- API/routes/backend_routes.py
+++ API/routes/backend_routes.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from tools.backend_route_tools import add_hazards, add_controls, get_user_topic, add_action
+from fastapi.responses import FileResponse
 
 router = APIRouter()
+
 
 @router.post("/api/add-hazard")
 def new_hazards(topic: str, action: str):
@@ -14,12 +16,19 @@ def new_controls(topic: str, action: str, hazard: str):
     controls_list = add_controls(topic=topic, action=action, hazard=hazard)
     return {"controls_list": controls_list}
 
+
 @router.post("/api/set-user-topic")
 def user_topic(topic: str):
     topic = get_user_topic(topic)
     return {"topic": topic}
 
+
 @router.post("/api/add-action-step")
 def new_step(action: str):
     action = add_action(action)
     return {"action": action}
+
+
+@router.get("/")
+def html_root(request: Request):
+    return FileResponse("web/index.html")
